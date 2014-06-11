f9900c9dbc6e121d3b36a1885c142f74ae2e3a8a
Opak Alex <opak.alexandr@gmail.com>
2014-06-11 14:37:14 +0300

    add static pages

--- app/router.js
+++ app/router.js
@@ -1,7 +1,11 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('index', {path: '/'})
+  this.route('index', {path: '/'});
+  this.route('team');
+  this.route('news');
+  this.route('contacts');
+  this.route('technologies');
 });
 
 export default Router;
